5d75c20d64de25613520756034b8f67a167b1588
Dr. Dhaval Patel <drdhaval2785@gmail.com>
2015-12-08 17:21:43 +0530

    compound study started

---
+++ compoundstudy.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Read data from sanhw2.txt and compare it against
+dictionary MD.
+When a word is possibly a compound, it is stored in 
+compoundhw.txt file.
+This would require a cursory checking.
+
+"""
+import sys, re
+import codecs
+import string
+import datetime
+import itertools
+from lxml import etree
+from io import StringIO, BytesIO
+import re
+from math import log
+import split as sp
+
+def dictlnumback(dicts,lnums):
+	output = ''
+	for i in xrange(len(dicts)):
+		output += ","+dicts[i]+";"+lnums[i]
+	output = output.strip(',')
+	return output
+def definitecompound(inputword,dictionary):
+	comp = sp.infer_spaces(inputword,dictionary)
+	splits = comp.split('+')
+	if len(splits) > 1:
+		splitlen = [len(member) for member in splits]
+		if min(splitlen) > 3:
+			return (inputword,comp)
+		else:
+			return ('','')
+	else:
+		return ('','')
+		
+	
+if __name__=="__main__":
+	print "Creating sanhw2 data"
+	sanhw2 = sp.sanhw2()
+	print "Created sanhw2 data"
+	fout = codecs.open('compoundstudy/compoundhw.txt','w','utf-8')
+	dictionary = 'dicts/MD.txt'
+	if len(sys.argv) == 2:
+		dictionary = 'dicts/'+sys.argv[1]+'.txt'
+	for (word,dicts,lnums) in sanhw2:
+		(hw,split) = definitecompound(word,dictionary)
+		if hw is not '':
+			print hw, '-', split
+			fout.write(hw+":"+split+":"+dictlnumback(dicts,lnums)+"\n")
+	fout.close()	
+	
+	
+	
+		
+	
