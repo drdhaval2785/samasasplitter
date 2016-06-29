3e9e88d56f757bd777a7dc05f1e53797c4677cd2
drdhaval2785 <drdhaval2785@gmail.com>
2016-06-29 10:20:32 +0530

    trial.py getting better

--- trial.py
+++ trial.py
@@ -1,7 +1,6 @@
 import itertools, timeit, datetime, sys
 lstrep = [('A',('A','aa','aA','Aa','AA','As')),('I',('I','ii','iI','Ii','II')),('U',('U','uu','uU','Uu','UU')),('F',('F','ff','fx','xf','Fx','xF','FF')),('e',('e','ea','ai','aI','Ai','AI')),('o',('o','oa','au','aU','Au','AU','aH','aHa','as')),('E',('E','ae','Ae','aE','AE')),('O',('O','ao','Ao','aO','AO')),('ar',('af','ar')),('d',('t','d')),('H',('H','s')),('S',('S','s','H')),('M',('m','M')),('y',('y','i','I')),('N',('N','M')),('Y',('Y','M')),('R',('R','M')),('n',('n','M')),('m',('m','M')),('v',('v','u','U')),('r',('r','s','H')),]
 lstrep1 = [('A',(('a','a'),('a','A'),('A','a'),('A','A'),('As',''))),('I',(('i','i'),('i','I'),('I','i'),('I','I'))),('U',(('u','u'),('u','U'),('U','u'),('U','U'))),('F',(('f','f'),('f','x'),('x','f'),('F','x'),('x','F'),('F','F'))),('e',(('e','a'),('a','i'),('a','I'),('A','i'),('A','I'))),('o',(('o','a'),('a','u'),('a','U'),('A','u'),('A','U'),('aH',''),('aH','a'),('as',''))),('E',(('a','e'),('A','e'),('a','E'),('A','E'))),('O',(('a','o'),('A','o'),('a','O'),('A','O'))),('ar',(('a','f'),)),('d',(('t',''),)),('H',(('s',''),)),('S',(('s',''),('H',''))),('M',(('m',''),)),('y',(('i',''),('I',''))),('N',(('M',''),)),('Y',(('M',''),)),('R',(('M',''),)),('n',(('M',''),)),('m',(('M',''),)),('v',(('u',''),('U',''))),('r',(('s',''),('H','')))]
-print len(lstrep1)
 dicti = 'dicts/mwb.txt'
 dictionary = open(dicti).read().split()
 def starts(word,list):
@@ -33,24 +32,33 @@ def reps(letter,lstrep1):
 			output.append(''.join(lst))
 	return output
 
-def permut(word,lstrep,dictionary,allowed):
-	dictset = set(dictionary)
-	input_str = word
-	subs = []
-	for i, c in enumerate(input_str):
-		for (a,b) in lstrep:
-			if c == a:
-				subs.append( (i, (a,b)))
+def permut(word):
+	global lstrep1, dictionary
 	# build output by applying each sub recorded
 	out = []
-	allowed = sorted(allowed, key=lambda x:len(x), reverse=True)
-	for allow in allowed:
-		entr = [entr for (a,(b,entr)) in subs if a==len(allow)-1]
-		print allow[:-1], len(allow), entr
+	if word in dictionary:
+		out.append(word)
+	else:
+		allowed = st1(inputword,dictionary)
+		dictset = set(dictionary)
+		input_str = word
+		subs = []
+		for i, c in enumerate(input_str):
+			for (a,b) in lstrep1:
+				if c == a:
+					subs.append( (i, (a,b)))
+		allowed = sorted(allowed, key=lambda x:len(x), reverse=True)
+		for allow in allowed:
+			entry = [entr for (a,(b,entr)) in subs if a==len(allow)-1]
+			if len(entry) > 0:
+				eligentries =  [(a,b) for (a,b) in entry[0] if allow[-1]==a]
+				if len(eligentries) > 0:
+					for eligen in eligentries:
+						out.append((len(allow), allow[:-1]+eligen[0], eligen[1]+word[len(allow):]))
+	return out
 if __name__=="__main__":
 	print datetime.datetime.now()
 	inputword = sys.argv[1]
-	allowed = st1(inputword,dictionary)
-	print permut(inputword,lstrep1,dictionary,allowed)
+	print permut(inputword)
 	print datetime.datetime.now()
 	
